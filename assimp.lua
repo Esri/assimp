2110999e0c0ec373b728e05089c75a831bfb9cd9
Jeremy Sigrist <jsigrist@esri.com>
2019-09-10 13:01:00 -0700

    Add assimp.lua file (ifc disabled)

---
+++ assimp.lua
@@ -0,0 +1,522 @@
+project "assimp"
+
+  local prj = project()
+  local prjDir = prj.basedir
+
+  -- -------------------------------------------------------------
+  -- project
+  -- -------------------------------------------------------------
+
+  -- common project settings
+
+  dofile (_BUILD_DIR .. "/3rdparty_static_project.lua")
+
+  -- project specific settings
+
+  uuid "BA8CFAE5-30DB-4480-909F-2FF454EFA675"
+
+  flags {
+    "NoPCH",
+  }
+
+  -- The formats we want are 3DS, DAE, OBJ, GLTF, FBX, DXF, X, STL, PLY, BLEND
+  defines {
+    "ASSIMP_BUILD_NO_OWN_ZLIB",
+    "ASSIMP_BUILD_NO_EXPORT",
+    "ASSIMP_BUILD_NO_3D_IMPORTER",
+    "ASSIMP_BUILD_NO_IFC_IMPORTER",
+    "ASSIMP_BUILD_NO_3MF_IMPORTER",
+    "ASSIMP_BUILD_NO_AC_IMPORTER",
+    "ASSIMP_BUILD_NO_AMF_IMPORTER",
+    "ASSIMP_BUILD_NO_ASE_IMPORTER",
+    "ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+    "ASSIMP_BUILD_NO_B3D_IMPORTER",
+    "ASSIMP_BUILD_NO_BASE_IMPORTER",
+    "ASSIMP_BUILD_NO_BVH_IMPORTER",
+    "ASSIMP_BUILD_NO_C4D_IMPORTER",
+    "ASSIMP_BUILD_NO_CSM_IMPORTER",
+    "ASSIMP_BUILD_NO_HMP_IMPORTER",
+    "ASSIMP_BUILD_NO_IRRMESH_IMPORTER",
+    "ASSIMP_BUILD_NO_IRR_IMPORTER",
+    "ASSIMP_BUILD_NO_LWO_IMPORTER",
+    "ASSIMP_BUILD_NO_LWS_IMPORTER",
+    "ASSIMP_BUILD_NO_MD2_IMPORTER",
+    "ASSIMP_BUILD_NO_MD3_IMPORTER",
+    "ASSIMP_BUILD_NO_MD5_IMPORTER",
+    "ASSIMP_BUILD_NO_MDC_IMPORTER",
+    "ASSIMP_BUILD_NO_MDL_IMPORTER",
+    "ASSIMP_BUILD_NO_MMD_IMPORTER",
+    "ASSIMP_BUILD_NO_MS3D_IMPORTER",
+    "ASSIMP_BUILD_NO_NDO_IMPORTER",
+    "ASSIMP_BUILD_NO_NFF_IMPORTER",
+    "ASSIMP_BUILD_NO_OFF_IMPORTER",
+    "ASSIMP_BUILD_NO_OGRE_IMPORTER",
+    "ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+    "ASSIMP_BUILD_NO_Q3BSP_IMPORTER",
+    "ASSIMP_BUILD_NO_Q3D_IMPORTER",
+    "ASSIMP_BUILD_NO_RAW_IMPORTER",
+    "ASSIMP_BUILD_NO_SIB_IMPORTER",
+    "ASSIMP_BUILD_NO_SMD_IMPORTER",
+    "ASSIMP_BUILD_NO_TERRAGEN_IMPORTER",
+    "ASSIMP_BUILD_NO_X3D_IMPORTER",
+    "ASSIMP_BUILD_NO_XGL_IMPORTER",
+    "ZIP_DEFAULT_COMPRESSION_LEVEL=6",
+  }
+
+  files {
+    "code/**.cpp",
+    "code/**.h",
+    "code/**.hpp",
+    "contrib/clipper/**.cpp",
+    "contrib/clipper/**.hpp",
+    "contrib/irrXML/**.cpp",
+    "contrib/irrXML/**.h",
+    "contrib/poly2tri/poly2tri/**.cc",
+    "contrib/poly2tri/poly2tri/**.h",
+  }
+
+  includedirs {
+    "include",
+    "code",
+    "contrib/irrXML",
+    "contrib/rapidjson/include",
+    "contrib/openddlparser/include",
+    prjDir,
+    prjDir .. "/../boost",
+    prjDir .. "/../zlib",
+  }
+
+  -- -------------------------------------------------------------
+  -- configurations
+  -- -------------------------------------------------------------
+
+  if (os.is("windows") and not _TARGET_IS_WINUWP) then
+    -- -------------------------------------------------------------
+    -- configuration { "windows" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/3rdparty_static_win.lua")
+
+    -- project specific configuration settings
+
+    configuration { "windows" }
+
+      buildoptions {
+        "/bigobj", -- Allow large object files for unity builds and template heavy objects
+      }
+
+    -- -------------------------------------------------------------
+    -- configuration { "windows", "Debug", "x32" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_win_x86_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "windows", "Debug", "x32" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "windows", "Debug", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_win_x64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "windows", "Debug", "x64" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "windows", "Release", "x32" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_win_x86_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "windows", "Release", "x32" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "windows", "Release", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_win_x64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "windows", "Release", "x64" }
+
+    -- -------------------------------------------------------------
+  end
+
+  if (os.is("linux") and not _OS_IS_ANDROID) then
+    -- -------------------------------------------------------------
+    -- configuration { "linux" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_linux.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "linux" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "linux", "Debug", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_linux_x64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "linux", "Debug", "x64" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "linux", "Release", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_linux_x64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "linux", "Release", "x64" }
+
+    -- -------------------------------------------------------------
+  end
+
+  if (os.is("macosx") and not _OS_IS_IOS and not _OS_IS_ANDROID) then
+    -- -------------------------------------------------------------
+    -- configuration { "macosx" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_mac.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "macosx" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "macosx", "Debug", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_mac_x64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "macosx", "Debug", "x64" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "macosx", "Release", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_mac_x64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "macosx", "Release", "x64" }
+
+    -- -------------------------------------------------------------
+  end
+
+  if (_OS_IS_IOS) then
+    -- -------------------------------------------------------------
+    -- configuration { "ios" } == _OS_IS_IOS
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_ios.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "ios*" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "ios_arm64_debug" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_ios_arm64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "ios_arm64_debug" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "ios_arm64_release" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_ios_arm64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "ios_arm64_release" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "ios_sim64_debug" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_ios_sim64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "ios_sim64_debug" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "ios_sim64_release" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_ios_sim64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "ios_sim64_release" }
+
+    -- -------------------------------------------------------------
+  end
+
+  if (_OS_IS_ANDROID) then
+    -- -------------------------------------------------------------
+    -- configuration { "android" } == _OS_IS_ANDROID
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_android.lua")
+
+    -- project specific configuration settings
+
+    configuration { "android*" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "android_armv7_debug" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_android_armv7_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "android_armv7_debug" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "android_armv7_release" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_android_armv7_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "android_armv7_release" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "android_x86_debug" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_android_x86_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "android_x86_debug" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "android_x86_release" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_android_x86_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "android_x86_release" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "android_arm64_debug" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_android_arm64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "android_arm64_debug" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "android_arm64_release" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_android_arm64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "android_arm64_release" }
+
+    -- -------------------------------------------------------------
+  end
+
+  if (_TARGET_IS_WINUWP) then
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp" } == _TARGET_IS_WINUWP
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp.lua")
+
+    -- project specific configuration settings
+
+    configuration { "windows" }
+
+      defines {
+        "_CRT_SECURE_NO_WARNINGS",
+      }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_debug", "x32" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_x86_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_debug", "x32" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_release", "x32" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_x86_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_release", "x32" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_debug", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_x64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_debug", "x64" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_release", "x64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_x64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_release", "x64" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_debug", "ARM" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_arm_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_debug", "ARM" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_release", "ARM" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_arm_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_release", "ARM" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_debug", "ARM64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_arm64_debug.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_debug", "ARM64" }
+
+    -- -------------------------------------------------------------
+    -- configuration { "winuwp_release", "ARM64" }
+    -- -------------------------------------------------------------
+
+    -- common configuration settings
+
+    dofile (_BUILD_DIR .. "/static_winuwp_arm64_release.lua")
+
+    -- project specific configuration settings
+
+    -- configuration { "winuwp_release", "ARM64" }
+
+    -- -------------------------------------------------------------
+  end
+
+  if (_IS_QT) then
+    -- -------------------------------------------------------------
+    -- configuration { "qt" }
+    -- -------------------------------------------------------------
+
+    local qt_include_dirs = PROJECT_INCLUDE_DIRS
+
+    -- Add additional QT include dirs
+    -- table.insert(qt_include_dirs, <INCLUDE_PATH>)
+
+    createqtfiles(project(), qt_include_dirs)
+
+    -- -------------------------------------------------------------
+  end
