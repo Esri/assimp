e34639586f2940bee27cc4d4c652ce818c6497e8
Rajvinder Singh <rajvinder_singh@esri.com>
2025-01-14 12:33:10 -0800

    Include revision.h file:
    Location of  revision.h file has changed in assimp code
    https://github.com/assimp/assimp/pull/5615/files
    
    Need to include it in the source.
    
    Remove old revision.h:

--- include/assimp/revision.h
+++ include/assimp/revision.h
@@ -1,12 +1,12 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x8134cae
+#define GitVersion 0x8cd1a535
 #define GitBranch "runtimecore"
 
 #define VER_MAJOR 5
-#define VER_MINOR 1
-#define VER_PATCH 6
+#define VER_MINOR 4
+#define VER_PATCH 3
 #define VER_BUILD 0
 
 #define STR_HELP(x) #x
@@ -16,13 +16,14 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit @GIT_COMMIT_HASH@)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 8cd1a535)"
 #endif
+#define VER_COPYRIGHT_STR           "\xA9 2006-2023"
 
 #ifdef  NDEBUG
-#define VER_ORIGINAL_FILENAME_STR   "assimp.dll"
+#define VER_ORIGINAL_FILENAME_STR   "assimp-vc143-mt.dll"
 #else
-#define VER_ORIGINAL_FILENAME_STR   "assimp.dll"
+#define VER_ORIGINAL_FILENAME_STR   "assimp-vc143-mtd.dll"
 #endif //  NDEBUG
 
 #endif // ASSIMP_REVISION_H_INC
