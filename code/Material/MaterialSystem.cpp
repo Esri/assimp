90832c2907199be3f64e37e9fd034b62f62ee71c
Rajvinder Singh <rajvinder_singh@esri.com>
2025-08-15 10:28:29 -0700

    Revert "Merge pull request #22 from Esri/cont_rajv/expose_extras_from_glTF_material"
    This reverts commit d76ca43d9f0c3df7eb22239163e2c6a5c8d3ed09, reversing
    changes made to e6d1d8d173a6444cd17be92f64cae70469f4e5a9.

--- code/Material/MaterialSystem.cpp
+++ code/Material/MaterialSystem.cpp
@@ -49,7 +49,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <assimp/ParsingUtils.h>
 #include <assimp/fast_atof.h>
 #include <assimp/material.h>
-#include <assimp/metadata.h>
 #include <assimp/types.h>
 #include <assimp/DefaultLogger.hpp>
 #include <memory>
@@ -422,13 +421,6 @@ unsigned int aiGetMaterialTextureCount(const C_STRUCT aiMaterial *pMat, C_ENUM a
     return max;
 }
 
-// ---------------------------------------------------------------------------
-// Get the metadata for the material
-C_STRUCT aiMetadata *aiGetMaterialMetadata(const C_STRUCT aiMaterial *pMat) {
-    ai_assert(pMat != nullptr);
-    return pMat->mMetaData;
-}
-
 // ------------------------------------------------------------------------------------------------
 aiReturn aiGetMaterialTexture(const C_STRUCT aiMaterial *mat,
         aiTextureType type,
@@ -486,7 +478,7 @@ static const unsigned int DefaultNumAllocated = 5;
 // ------------------------------------------------------------------------------------------------
 // Construction. Actually the one and only way to get an aiMaterial instance
 aiMaterial::aiMaterial() :
-        mProperties(nullptr), mNumProperties(0), mNumAllocated(DefaultNumAllocated), mMetaData(nullptr) {
+        mProperties(nullptr), mNumProperties(0), mNumAllocated(DefaultNumAllocated) {
     // Allocate 5 entries by default
     mProperties = new aiMaterialProperty *[DefaultNumAllocated];
 }
@@ -496,7 +488,6 @@ aiMaterial::~aiMaterial() {
     Clear();
 
     delete[] mProperties;
-    delete mMetaData;
 }
 
 // ------------------------------------------------------------------------------------------------
